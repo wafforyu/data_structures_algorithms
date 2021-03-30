6518fc8b1578a4e849ebd29a2560738ebb2d038b
wafforyu <wafforyu@gmail.com>
2021-03-30 23:56:47 +0800

    bubble sort

---
+++ javascript/bubble.js
@@ -0,0 +1,14 @@
+//bubble sort
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) { 
+      if(array[j] > array[j+1]) {
+        //Swap the numbers
+        let temp = array[j]
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }        
+  }
+}
