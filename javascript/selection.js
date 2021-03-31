d84e6092978d64b78922b77a42f01b0d0535d8e2
wafforyu <wafforyu@gmail.com>
2021-03-31 22:32:05 +0800

    selection sort

---
+++ javascript/selection.js
@@ -0,0 +1,17 @@
+function selectionSort(array) {
+  const length = array.length;
+  for(let i = 0; i < length; i++){
+    // set current index as minimum
+    let min = i;
+    let temp = array[i];
+    for(let j = i+1; j < length; j++){
+      if (array[j] < array[min]){
+        //update minimum if current is lower that what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
