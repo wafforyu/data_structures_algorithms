e097fe70e92dcd895e96a3e5653de3f7550c065e
wafforyu <wafforyu@gmail.com>
2021-03-31 22:35:19 +0800

    linked list

---
+++ javascript/linkedlist.js
@@ -0,0 +1,13 @@
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    //Code here
+  }
+}
