48cd3dad592c2a2737f49a9d2b54631f316a0ebc
wafforyu <wafforyu@gmail.com>
2021-04-02 10:53:52 +0700

    #

---
+++ javascript/linkedlist.js
@@ -0,0 +1,33 @@
+//TO BE COMPLETED
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+    append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    console.log(newNode)
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+    prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
