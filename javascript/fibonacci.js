c2cd29434fce638e635536efbd8bfab28eb5c74f
= <=>
2021-04-04 00:01:00 +0800

    more organization

--- javascript/fibonacci.js
+++
@@ -1,4 +0,0 @@
-function fibonacciRecursive(n) {
-  if (n < 2) return n;
-  return fibonacciRecursive(n - 1) + fibonacciRecursive (n - 2);
-}
--- javascript/recursion.js
+++
@@ -1,7 +0,0 @@
-function countdown(num){
-    if(num === 0) return 0; //base case
-    console.log(num);
-    num--;
-    return countdown(num); 
-}
-console.log(countdown(5));
