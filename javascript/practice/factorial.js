61959963e986a2db81057aa8cde510a4a346bbd6
wafforyu <wafforyu@gmail.com>
2021-04-15 22:06:38 +0800

    add factorial iterative comparison

--- javascript/practice/factorial.js
+++ javascript/practice/factorial.js
@@ -4,3 +4,15 @@ function findFactorialRecursive(number) {
   }
   return number * findFactorialRecursive(number - 1)
 }
+
+function findFactorialIterative(number){
+  let total = 1;
+  for (let i = 2; i <= number; i++) {
+    total *= i;
+  }
+  return total;
+}
+
+let a = findFactorialIterative(5);
+let b = findFactorialRecursive(5);
+console.log(a,b);
