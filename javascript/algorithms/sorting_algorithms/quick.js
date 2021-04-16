c60665ab1662d4f16d44fa1a223e2eb28caa0829
wafforyu <wafforyu@gmail.com>
2021-04-16 21:48:09 +0800

    quicksort progress

--- javascript/algorithms/sorting_algorithms/quick.js
+++ javascript/algorithms/sorting_algorithms/quick.js
@@ -3,23 +3,24 @@
 //IN PROGRESS
 //IN PROGRESS
 //IN PROGRESS
-function quickSort(array,start=0, end=array.length+1){
-  if(start < end){
-  let pivotIndex = pivotHelper(array, start, end);
-  quickSort(array, start, pivotIndex-1);
-  quickSort(array, pivotIndex+1, end);
-  } 
-    return array;
+function quickSort(array,start=0, end=array.length-1){
+  if (start < end){
+    let pivotIndex = pivotHelper(array, start, end);
+    quickSort(array, start, pivotIndex-1);
+    quickSort(array, pivotIndex+1, end);
+  }
+  return array;
 }
 
-function pivotHelper(array, start=0, end=array.length-1){
+function pivotHelper(array, start=0, end=array.length+1){
   let pivot = array[start];
   let ctr = start;  
   for(var i = start+1; i < end; i++ )
     if(pivot > array[i]){
       ctr++;
-      array = swap(array, start, i);
+      swap(array, start, i);
     }
+  swap(array, start, i);
   return ctr;
 }
 
@@ -27,11 +28,11 @@ function swap(array, start, i){
   let temp = array[start];
   array[start] = array[i];
   array[i] = temp;
-  return array;
 }
 
 let array = [9,7,4,3,6,7,8];
-console.log(quickSort(array))
+
+console.log(quickSort(array));
 //function quickSort(array, left, right){
 //  const len = array.length; 
 //  let pivot;
