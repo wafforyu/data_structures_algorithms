49da09a072344774ee09800631a3ed1748249154
wafforyu <wafforyu@gmail.com>
2021-05-10 23:30:40 +0800

    add linked list in python from geeks for geeks

---
+++ python/main.py
@@ -0,0 +1,13 @@
+import data_structures.linked_list
+
+
+if __name__ == '__main__':
+    n = data_structures.linked_list.LinkedList()
+
+    n.push(5)
+    n.push(4)
+    n.push(3)
+    n.push(2)
+    n.push(1)
+
+    n.printList()
