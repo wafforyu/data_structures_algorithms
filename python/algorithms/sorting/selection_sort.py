324a46fa9801decceb50f2193ff67d29a0c18e7a
wafforyu <wafforyu@gmail.com>
2021-05-14 21:08:21 +0800

    add selection sort in python

---
+++ python/algorithms/sorting/selection_sort.py
@@ -0,0 +1,16 @@
+# selection sort
+
+def selection_sort(arr):
+    i = 0
+    while i < len(arr):  # while the entire list is not through
+        lowest = i  # set initial lowest value to first index[0]
+
+        j = i + 1  # set j pointer to start 1 index ahead of i
+        while j < len(arr):  # j pointer must go through all the elements ahead of i
+            if arr[i] > arr[j]:  # if the current element is smaller than the lowest (i)
+                lowest = j  # update the new lowest
+                arr[i], arr[j] = arr[j], arr[i]  # swap the elements
+            j += 1  # increment to continue until the loop is over
+        i += 1
+
+    return arr
--- python/main.py
+++ python/main.py
@@ -1,17 +1,4 @@
-from data_structures.linked_list.linked_list_review import SinglyLinkedList
+import data_structures
 
 if __name__ == '__main__':
-    n = SinglyLinkedList()
-
-    n.push(5)
-    n.push(4)
-    n.push(3)
-    n.push(2)
-    n.push(1)
-
-    n.traverse()
-    print()
-    n.insertAfter(2, 0)
-
-    n.traverse()
-
+    print("Hello world")
